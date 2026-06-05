b65e02d94fb8c69904a2ac280be2f31f1a4309f4
jfag <jfag@cin.ufpe.br>
2026-06-05 15:38:38 -0300

    add main e movimentação

---
+++ mainCode.py
@@ -0,0 +1,47 @@
+import pygame
+import numpy
+from player import Jogador
+import sys
+import os
+
+pygame.init() 
+clock = pygame.time.Clock()
+#tamanhoTela:tuple = pygame.display.get_desktop_sizes()[0]
+telaSizePlaceholder = (1360,800)
+tela = pygame.display.set_mode(telaSizePlaceholder)
+fps=60
+bg = pygame.image.load(os.path.join("projeto","images","placeholderBG.png")).convert()
+bg = pygame.transform.scale(bg, (1360,800))
+bgSize = bg.get_rect()
+main = True
+
+jogador = Jogador(
+        spriteImage=os.path.join("projeto",'images', 'playerSprites', 'slime_green.png'),
+        posInicial=(1360 / 2, 800),
+        #grupos=self.all_sprites,
+        #game=self
+    )
+
+while main:
+    #ve se fechou o jogo
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            main=False
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord("q"):
+                pygame.quit()
+                sys.exit()
+                main=False
+    
+    #limpa tela pra atualizar prox frame
+    scaling=5
+    tela.blit(bg, bgSize)
+    grupoJogador = pygame.sprite.Group()
+    grupoJogador.add(jogador)
+    jogador.update()
+    grupoJogador.draw(tela)
+    #flip atualiza a tela
+    pygame.display.flip()
+    clock.tick(fps)
---
+++ player.py
@@ -0,0 +1,56 @@
+import pygame
+import numpy
+import os
+
+#tamanhoTela:tuple = pygame.display.get_desktop_sizes()[0]
+
+class Jogador(pygame.sprite.Sprite):
+    
+    def __init__(self, spriteImage, posInicial):
+        
+        super().__init__()
+        
+        self.images = []
+        self.sheet = pygame.image.load(spriteImage).convert_alpha()
+        sheetSize = self.sheet.get_rect()
+        self.direction=pygame.math.Vector2()
+        self.animacoes = self.fatiar_spritesheet(self.sheet)
+        self.estadoAnimacao = "run"
+        self.frameAtual = 0
+        self.velocidade = 50
+        self.image = pygame.image.load(os.path.join("projeto", "images", "playerSprites", "climb-0.png")).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.posicao = pygame.math.Vector2(self.rect.center)
+        
+    
+    def fatiar_spritesheet(self,sheet):
+        larguraSprite=32
+        alturaSprite=24
+        animacoes = {"run":[]}
+        for linha in range(2):
+            if linha==1:
+                for coluna in range(3):
+                    x = linha*larguraSprite
+                    y = coluna*alturaSprite
+                    sprite = sheet.subsurface(pygame.Rect(x,y, larguraSprite, alturaSprite))
+                    sprite = pygame.transform.scale(sprite, (256, 192))
+                    animacoes["run"].append(sprite)
+        return animacoes
+            
+    def getDirection(self):
+        moveKeys = pygame.key.get_pressed()
+        self.direction.x = int(moveKeys[pygame.K_d]) - int(moveKeys[pygame.K_a])
+        self.direction.y = int(moveKeys[pygame.K_s]) - int(moveKeys[pygame.K_w])
+        #pelo jeito da erro se tentar normalizar um vetor (0,0)
+        if self.direction != (0,0):
+            self.direction = self.direction.normalize()
+    
+    def movimentacao(self):
+        print(self.direction)
+        self.posicao += self.direction * self.velocidade
+        self.rect.centerx = self.posicao.x
+        self.rect.centery = self.posicao.y
+    
+    def update(self):
+        self.getDirection()
+        self.movimentacao()
