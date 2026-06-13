79f68d1883dfba4be00e4e15a4cc4a5a3a9155b8
jfag <jfag@cin.ufpe.br>
2026-06-13 00:23:42 -0300

    mod in rajada and add tracker enemy

--- enemy.py
+++ enemy.py
@@ -6,7 +6,8 @@ clock = pygame.time.Clock()
 inimigos_data = {
             0: {"imagem" : "retangulo_vermelho.png", "velocidade" :(700, 250), "vida" : 200, "bala" : "follow"}, 
             1: {"imagem" : "hexagono_amarelo2.png", "velocidade" : (300, 250), "vida": 300, "bala": "rajada"},
-            2: {"imagem" : "quadrado_roxo.png", "velocidade": (800, 200), "vida": 100, "bala": "bigger"}
+            2: {"imagem" : "quadrado_roxo.png", "velocidade": (800, 200), "vida": 100, "bala": "bigger"},
+            3 :{"imagem" : "retangulo_cinza.png", "velocidade": (500, 200), "vida": 250, "bala": "tracker"}
             }
 
 class Inimigo(pygame.sprite.Sprite):
@@ -28,7 +29,7 @@ class Inimigo(pygame.sprite.Sprite):
         self.direcao = pygame.Vector2()
         self.posicao = pygame.Vector2(self.rect.centerx, self.rect.centery)
         self.vida = inimigos_data[i]["vida"]
-        self.limites_mov = limites_mov #padrão --> (x0, x1, y0, y1)
+        self.limites_mov = limites_mov #padrão --> (x0, x1)
         self.sentido_inicial = sentido_inicial
         self.tipo_bala = inimigos_data[i]["bala"]
         self.disparo = 1
@@ -111,7 +112,7 @@ class Bullet(pygame.sprite.Sprite):
     
     def __init__(self, image, posicao, dt, tipo):
         folderPath = os.path.dirname(os.path.abspath(__file__))
-        velocidades = {"follow": 600, "rajada": 500, "bigger": 400}
+        velocidades = {"follow": 600, "rajada": 500, "bigger": 400, "tracker": 300}
         #print("teste 1")
         super().__init__()
         self.dt = dt
@@ -132,7 +133,7 @@ class Bullet(pygame.sprite.Sprite):
         dx = (posA[0] - posB[0])
         dy = (posA[1] - posB[1])
         
-        if self.tipo == "follow" or self.tipo == "bigger":
+        if self.tipo == "follow" or self.tipo == "bigger" or self.tipo == "tracker":
             if dx !=0: #para evitar divisao por zero    
                 ang = (math.atan(dy/dx))
             else:
@@ -148,17 +149,17 @@ class Bullet(pygame.sprite.Sprite):
             if dy <0:
                 dy = -dy
             #print(pow)
-            indicies = {"b0" : (-300, posB[0]), "b1" : (-150, posB[0]), "b2" : (0, posB[0]), "b3":(150, posB[0]), "b4" :(300, posB[0])}
-            if pow != 2:
+            indicies = {"b0" : (0, -posB[0]), "b1" : (posB[0], 0), "b2" : (-posB[0], 0), "b3":(0, posB[0]), "b4" :(118, 96), "b5": (118, -96), "b6": (-118, 96), "b7" :(-118, -96)}
+            if pow not in (0, 3):
                 ang = math.atan(indicies[f"b{pow}"][1]/indicies[f"b{pow}"][0])
             else:
               ang = (math.pi)/2
             cos = math.cos(ang)
             sin = math.sin(ang)
-            if pow in (0, 1): #correcao para os valores do cosseno negativo
+            if pow in (0, 2, 5, 7): #correcao para os valores do cosseno negativo
                 cos = -cos
-            if sin<0:  #correcao para o seno, para ele sempre atirar p baixo 
-                sin = -sin
+            """if sin<0:  #correcao para o seno, para ele sempre atirar p baixo 
+                sin = -sin"""
             self.dire = pygame.math.Vector2((cos*self.velocidade), (sin*self.velocidade))
             #print("AQUI porra"),print(self.dire)
 
@@ -189,12 +190,14 @@ class Bullet(pygame.sprite.Sprite):
         else:
             self.disparo =0
 
-    def update(self, dt, camera, playerPos):
+    def update(self, dt, camera, playerPos, mudar):
         self.mov(camera)
         self.dt = dt
         if abs(playerPos.x-self.posicao.x) >= 3000 or abs(playerPos.y-self.posicao.y) >= 3000:
             self.kill()
             #print("Dead")
+        if self.tipo == "tracker" and mudar:
+            self.direcao(playerPos, self.posicao, pow="null")
         
         
 
